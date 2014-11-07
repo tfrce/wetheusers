b422db1537e5b6ec0c35faaea16c4cd592a8ee65
Thomas Davis <thomasalwyndavis@gmail.com>
2014-11-08 00:08:31 +1000

    added html5 validation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 
 $(document).ready(function () {
+
+
+
+  // ------------ Events ------------- //
+
+  // Input animation
   $('form input').blur(function () {
     if( $(this).val() ) {
         $(this).addClass('filled');
@@ -7,13 +13,18 @@ $(document).ready(function () {
         $(this).removeClass();
       }
   });
+
+  // Expand the text button for mobile
   $('#expand').click(function() {
     $('main').addClass('expanded');
     return false;
   });
   
-  $('input#submit').click(function() {
+
+  // On form submissions
+  $('.petition-form').on('submit', function () {
     $('#modal').fadeIn();
     return false;
-  });
+  })
+
 });
